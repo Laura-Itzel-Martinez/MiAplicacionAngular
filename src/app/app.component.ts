a19711e15d8236ddac180e3318b7d57146ca3c66
Laura-Itzel-Martinez <itzelmartinez455@gmail.com>
2021-10-22 20:28:06 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'holaMundo';
+  public title:string  = 'Contador con angular';
+  public numero:number = 0;
+
+  /*public sumar(){
+    this.numero++;
+  }
+  public restar(){
+    this.numero--;
+
+  }*/
+
+  public sumarRestar(valor:number){
+      this.numero=this.numero + valor;
+  }
 }
